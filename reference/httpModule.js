861e93500190160f187624f52bcb29ac3f979ec6
Nguyen Thanh Minh <nguyenthanhminh112000@gmail.com>
2021-05-21 16:02:53 +0700

    http Module

---
+++ reference/httpModule.js
@@ -0,0 +1,10 @@
+const http = require('http');
+
+http
+  .createServer((req, res) => {
+    res.write('Hello World!');
+    res.end();
+  })
+  .listen(5000, () => {
+    console.log('Server is running ....');
+  });
