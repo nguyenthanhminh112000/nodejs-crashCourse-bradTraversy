89e06898188910911e51ec3f5ab3fbb94ba7d70d
Nguyen Thanh Minh <nguyenthanhminh112000@gmail.com>
2021-05-21 13:44:55 +0700

    os Module

---
+++ reference/osModule.js
@@ -0,0 +1,22 @@
+const os = require('os');
+
+// Platform
+console.log(os.platform());
+
+// Architecture
+console.log(os.arch());
+
+// CPU Core Info
+console.log(os.cpus());
+
+// free Memory
+console.log(os.freemem());
+
+// total Memory
+console.log(os.totalmem());
+
+// Home directory
+console.log(os.homedir());
+
+// uptime
+console.log(os.uptime());
